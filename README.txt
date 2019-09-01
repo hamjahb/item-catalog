6c37be533b1b47af28201edaf800cb970b2d0ff5
Hisham-PC <h.aljahbli@gmail.com>
2019-09-01 15:20:56 +0300

    added initial routing to application

--- README.txt
+++ README.txt
@@ -1,7 +1,12 @@
 Place your catalog project in this directory.
 
 technologies used:
+
+modules used:
 python 3.5.2
 Flask 1.1.1
 SQLAlchemy 1.3.6
 passlib 1.7.1
+flask_httpauth
+
+localhost:8000
--- application.py
+++ application.py
@@ -15,7 +15,42 @@ session = DBSession()
 app = Flask(__name__)
 
 
+@app.route('/')
+@app.route('/catalog')
+def showLatestItems():
+    return 'this page will show the main page with latest added items'
 
+@app.route('/categories')
+def showCategories():
+    return 'this will show all available categories'
+
+@app.route('/categories/new')
+def newCategory():
+    return 'this page will add a new category'
+
+@app.route('/categories/<int:category_id>/edit')
+def editCategory(category_id):
+    return 'this page will be to edit {category_id}'
+
+@app.route('/categories/<int:category_id>/delete')
+def deleteCategory(category_id):
+    return 'this page will be to delete {category_id}'
+
+@app.route('/categories/<int:category_id>/items')
+def showCategoryItems(category_id):
+    return 'will show all items in {category_id}'
+
+@app.route('/categories/<int:category_id>/new')
+def newCategoryItem(category_id):
+    return 'this page will adda new item in {category_id}'
+
+@app.route('/categories/<int:category_id>/<int:item_id>/edit')
+def editCategoryItem(category_id, item_id):
+    return 'this page will be to edit {item_id} in {category_id}'
+
+@app.route('/categories/<int:category_id>/<int:item_id>/delete')
+def deleteCategoryItem(category_id, item_id):
+    return 'this page will be to delete {item_id} in {category_id}'
 
 
 
